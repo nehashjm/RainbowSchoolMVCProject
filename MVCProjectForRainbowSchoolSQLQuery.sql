27447aa1e229eb7f58c898683e31c41626e72ae7
nehashjm <78907087+nehashjm@users.noreply.github.com>
2023-12-28 18:16:20 +0530

    Add files via upload

---
+++ MVCProjectForRainbowSchoolSQLQuery.sql
@@ -0,0 +1,25 @@
+create database RainbowSchoolDb
+
+use RainbowSchoolDb
+
+CREATE TABLE Subjects (
+    SubjectID INT PRIMARY KEY,
+    SubjectName NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Classes (
+    ClassID INT PRIMARY KEY,
+    ClassName NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    FirstName NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    ClassID INT FOREIGN KEY REFERENCES Classes(ClassID)  on delete cascade on update cascade,
+    SubjectID INT FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID)  on delete cascade on update cascade,
+);
+
+select * from Student
+select * from Subjects
+select * from Classes
